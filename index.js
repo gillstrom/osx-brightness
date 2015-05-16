32448f226c33ab042e3beb0fa9e5ac2fd533192a
Andreas Gillström <andreasgillstrom@gmail.com>
2015-05-16 20:34:18 +0200

    Small tweak

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ module.exports = function (steps, cb) {
 		throw new Error('Only OS X are supported');
 	}
 
-	if (typeof steps !== 'number') {
+	if (!steps || isNaN(steps) || typeof steps !== 'number') {
 		throw new Error('Argument has to be a number');
 	}
 
